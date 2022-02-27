40d648f77df4512c33b59c9fc4c4b5ab8365327f
EvaMaeRey <evangeline.mae@gmail.com>
2022-02-27 12:56:26 -0500

    initial functions

---
+++ R/geom_text_twowaycount.R
@@ -0,0 +1,67 @@
+
+
+#' Title
+#'
+#' @param data
+#' @param scales
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # step 1b test the computation function
+#' library(dplyr)
+#' mtcars %>%
+#'   # input must have required aesthetic inputs as columns
+#'   rename(x = cyl, y = gear) %>%
+#'   compute_group_twowaycount() %>%
+#'   head()
+compute_group_twowaycount <- function(data, scales){
+
+  data %>%
+    # add an additional column called label
+    # the geom we inherit from requires the label aesthetic
+    dplyr::count(x, y) %>%
+    dplyr::rename(label = n)
+
+}
+
+
+StatTwowaycount <- ggplot2::ggproto(`_class` = "Twowaycount",
+                                  `_inherit` = ggplot2::Stat,
+                                  required_aes = c("x", "y"),
+                                  compute_group = compute_group_twowaycount)
+
+#' Title
+#'
+#' @param mapping
+#' @param data
+#' @param position
+#' @param na.rm
+#' @param show.legend
+#' @param inherit.aes
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' ggplot(data = mtcars) +
+#' aes(x = cyl, y = gear) +
+#' geom_text_twowaycount()
+geom_text_twowaycount <- function(mapping = NULL, data = NULL,
+                                  position = "identity", na.rm = FALSE,
+                                  show.legend = NA,
+                                  inherit.aes = TRUE, ...) {
+  ggplot2::layer(
+    stat = StatTwowaycount, # proto object from Step 2
+    geom = ggplot2::GeomText, # inherit other behavior
+    data = data,
+    mapping = mapping,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
---
+++ R/geom_tile_twowaycount.R
@@ -0,0 +1,68 @@
+
+
+#' Title
+#'
+#' @param data
+#' @param scales
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # step 1b test the computation function
+#' library(dplyr)
+#' library(magrittr)
+#' mtcars %>%
+#'   # input must have required aesthetic inputs as columns
+#'   rename(x = cyl, y = gear) %>%
+#'   compute_group_twowaycountfill() %>%
+#'   head()
+compute_group_twowaycountfill <- function(data, scales){
+
+  data %>%
+    # add an additional column called label
+    # the geom we inherit from requires the label aesthetic
+    dplyr::count(x, y) %>%
+    dplyr::rename(fill = n)
+
+}
+
+
+StatTwowaycountfill <- ggplot2::ggproto(`_class` = "Twowaycount",
+                                  `_inherit` = ggplot2::Stat,
+                                  required_aes = c("x", "y"),
+                                  compute_group = compute_group_twowaycountfill)
+
+#' Title
+#'
+#' @param mapping
+#' @param data
+#' @param position
+#' @param na.rm
+#' @param show.legend
+#' @param inherit.aes
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' ggplot(data = mtcars) +
+#' aes(x = cyl, y = gear) +
+#' geom_tile_twowaycount()
+geom_tile_twowaycount <- function(mapping = NULL, data = NULL,
+                                  position = "identity", na.rm = FALSE,
+                                  show.legend = NA,
+                                  inherit.aes = TRUE, ...) {
+  ggplot2::layer(
+    stat = StatTwowaycountfill, # proto object from Step 2
+    geom = ggplot2::GeomTile, # inherit other behavior
+    data = data,
+    mapping = mapping,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
